b66dca7439eac5aa104fb80b0bddb1dc6f9e30c6
suifengLuo <772396555@qq.com>
2014-11-26 11:58:34 +0800

    0.5

---
+++ Makefile
@@ -0,0 +1,11 @@
+export GOPATH :=/Users/suifengluo/Documents/gospace/gopro/
+export PATH := ${PATH}:${GOPATH}\bin
+export GOBIN := ${GOPATH}\bin
+main:
+	go run main.go
+
+pro:
+	go run gopro.go test
+
+build:
+	go install gopro.go
--- gopro.go
+++ gopro.go
@@ -5,26 +5,41 @@ import (
 	"os"
 )
 
+var (
+	subdir = []string{"bin", "pkg", "src"}
+
+	mainContent = []string{
+		"package main",
+		"import (",
+		"	\"fmt\"",
+		")",
+		"func main() {",
+		"	fmt.Println(\"project is ready!!!\")",
+		"}",
+	}
+
+	dockerContent = []string{
+		"FROM google/golang",
+		"WORKDIR /gopath",
+		"ADD . /gopath",
+		"RUN apt-get install -y vim",
+		"VOLUME /gopath",
+		"CMD [\"bash\"]",
+	}
+)
+
 func main() {
 
-	arg_num := len(os.Args)
-	if arg_num == 1 || arg_num > 2 {
-		fmt.Println("please add the project name！！！")
-		fmt.Println("like then :")
-		fmt.Println("    gopro <your project name>")
+	if checkArgErr() {
 		return
 	}
 
 	proName := os.Args[1]
-	var path string
-	if os.IsPathSeparator('\\') {
-		path = "\\"
-	} else {
-		path = "/"
-	}
+	path := getPathSeparator()
 	dir, _ := os.Getwd()
 	proPath := dir + path + proName
-	subdir := []string{"bin", "pkg", "src"}
+
+	//create subdir "src pkg bin"
 	for _, val := range subdir {
 		err := os.MkdirAll(proPath+path+val, os.ModePerm)
 		if err != nil {
@@ -33,34 +48,62 @@ func main() {
 		}
 	}
 
+	// create Makefile
 	mkfile := proPath + path + "Makefile"
-	mf, err := os.Create(mkfile)
-	if err != nil {
-		panic(err)
+	mkContent := []string{
+		"export GOPATH :=" + proPath,
+		"export PATH := ${PATH}:${GOPATH}" + path + "bin",
+		"export GOBIN := ${GOPATH}" + path + "bin",
+		"main:",
+		"	go run main.go",
+		"build:",
+		"	go install main.go",
 	}
-	mf.WriteString("export GOPATH :=" + proPath + "\n")
-	mf.WriteString("export PATH := ${PATH}:${GOPATH}\\bin\n")
-	mf.WriteString("export GOBIN := ${GOPATH}\\bin\n")
-	mf.WriteString("main:\n")
-	mf.WriteString("	go run main.go\n")
+	writeFile(mkfile, mkContent, "\n")
 
-	defer mf.Close()
-
+	//create main.go
 	mainfile := proPath + path + "main.go"
-	mainf, err := os.Create(mainfile)
-	if err != nil {
-		panic(err)
-	}
-	mainf.WriteString("package main\n")
-	mainf.WriteString("import (\n")
-	mainf.WriteString("	\"fmt\"\n")
-	mainf.WriteString(")\n")
-	mainf.WriteString("func main() {\n")
-	mainf.WriteString("	fmt.Println(\"project is ready!!!\")\n")
-	mainf.WriteString("}\n")
+	writeFile(mainfile, mainContent, "\n")
 
-	defer mainf.Close()
+	//create dockerfile
+	dockerFile := proPath + path + "Dockerfile"
+	writeFile(dockerFile, dockerContent, "\n")
 
 	fmt.Println("project is ready...")
 
+}
+
+func getPathSeparator() string {
+	var path string
+	if os.IsPathSeparator('\\') {
+		path = "\\"
+	} else {
+		path = "/"
+	}
+	return path
+}
+
+func checkArgErr() bool {
+	arg_num := len(os.Args)
+	if arg_num == 1 || arg_num > 2 {
+		fmt.Println("please add the project name！！！")
+		fmt.Println("like then :")
+		fmt.Println("    gopro <your project name>")
+		return true
+	}
+	return false
+}
+
+func writeFile(fNamePath string, content []string, postfix string) (err error) {
+
+	mf, err := os.Create(fNamePath)
+	defer mf.Close()
+	if err != nil {
+		return
+	}
+
+	for _, val := range content {
+		mf.WriteString(val + postfix)
+	}
+	return
 }
